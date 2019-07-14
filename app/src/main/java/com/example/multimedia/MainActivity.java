0b6eab00521942f2cbe57d3c2c8a3f5c66125443
ywk <987378653@qq.com>
2019-07-14 19:30:47 +0800

    增加视频播放

--- app/src/main/java/com/example/multimedia/MainActivity.java
+++ app/src/main/java/com/example/multimedia/MainActivity.java
@@ -11,17 +11,22 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.io.IOException;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.video_view)
+    VideoView videoView;
     private MediaPlayer mMediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +37,18 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
                 PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }else {
+        } else {
             initPlayMedia();
+            initVideoPath();
         }
     }
 
-    @OnClick({R.id.btn_start_play, R.id.btn_pause, R.id.btn_stop})
+    private void initVideoPath() {
+        File file = new File(Environment.getExternalStorageDirectory(), "movie.mp4");
+        videoView.setVideoPath(file.getPath());
+    }
+
+    @OnClick({R.id.btn_start_play, R.id.btn_pause, R.id.btn_stop, R.id.btn_start_video, R.id.btn_pause_video, R.id.btn_stop_video})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_start_play:
@@ -46,20 +57,40 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.btn_pause:
-                mMediaPlayer.pause();
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                }
                 break;
             case R.id.btn_stop:
-                mMediaPlayer.stop();
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+                break;
+            case R.id.btn_start_video:
+                if (!videoView.isPlaying()) {
+                    videoView.start();         //开始播放
+                }
+                break;
+            case R.id.btn_pause_video:
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+                break;
+            case R.id.btn_stop_video:
+                if (videoView.isPlaying()) {
+                    videoView.resume();
+                }
                 break;
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 1){
+        if (requestCode == 1) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 initPlayMedia();
-            }else {
+                initVideoPath();
+            } else {
                 Toast.makeText(this, "我们需要读取内存卡的权限，否则程序无法正常运行", Toast.LENGTH_SHORT).show();
             }
         }
@@ -81,5 +112,9 @@ public class MainActivity extends AppCompatActivity {
             mMediaPlayer.stop();
             mMediaPlayer.release();
         }
+        if (videoView != null) {
+            videoView.suspend();
+        }
     }
+
 }
